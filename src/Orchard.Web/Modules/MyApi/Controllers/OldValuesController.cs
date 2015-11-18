1516de7e74a3a7c6a93173ebf02b80e50a4919e9
Tom Pratt <tom@buzzinteractive.co.uk>
2015-11-18 14:03:32 +0000

    Added HttpAuth Orchard module as git submodule. Added a simple MyApi Orchard module to help test out HttpAuth.

---
+++ src/Orchard.Web/Modules/MyApi/Controllers/OldValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MyApi.Controllers
+{
+    [Authorize]
+    public class OldValuesController : ApiController
+    {
+        private readonly IMembershipService _membershipService;
+
+        public OldValuesController(
+            IMembershipService membershipService) {
+            _membershipService = membershipService;
+        }
+
+        private IUser _orchardUser;
+        public IUser OrchardUser {
+            get {
+                if (_orchardUser == null)
+                    _orchardUser = _membershipService.GetUser(User.Identity.Name);
+                return _orchardUser;
+            }
+        }
+
+        [Authorize]
+        public string Get() {
+            return "Hello, you are authorized. Id: " + OrchardUser.Id;
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/MyApi/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Orchard;
+using System.Web.Http;
+using Orchard.Core.Contents;
+
+namespace HttpAuth.Controllers
+{
+    public class ValuesController : ApiController {
+        private readonly IOrchardServices _orchardServices;
+
+        public ValuesController(IOrchardServices orchardServices) {
+            _orchardServices = orchardServices;
+        }
+
+        public IHttpActionResult Get() {
+            if (!_orchardServices.Authorizer.Authorize(Permissions.DeleteContent))
+                return Unauthorized();
+
+            return Ok("Your ID is " + _orchardServices.WorkContext.CurrentUser.Id + " and you are authorized to delete content");
+        }
+    }
+}
